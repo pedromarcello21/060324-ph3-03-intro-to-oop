b923e8511b53efe8bfe8e8b4d9d6872857fd951f
brewchetta <chett.tiller@gmail.com>
2024-04-23 12:01:44 -0400

    humans vs robots who would win

--- classes/robot.py
+++ classes/robot.py
@@ -1,3 +1,29 @@
 class Robot:
 
-    pass
+    # magic / dunder method on creating a new robot
+    def __init__(self, name:str, color:str, operating_system:str):
+        print("BUILDING A NEW ROBOT")
+        self.name = name
+        self.color = color
+        self.operating_system = operating_system
+
+    # magic / dunder method on looking at a robot directly (for example `print(r2d2)` )
+    def __repr__(self):
+        return f"Robot(name={self.name} color={self.color} operating_system={self.operating_system})"
+
+    # shows the robot itself
+    def see_myself(self):
+        return self
+        
+    # this method is called when we call r2d2.operating_system
+    @property
+    def operating_system(self):
+        return self._operating_system.upper()
+
+    # this method is called when we call r2d2.operating_system = ""
+    @operating_system.setter
+    def operating_system(self, value:str):
+        if type(value) == str:
+            self._operating_system = value
+        else:
+            raise TypeError("Robot operating_system must be a string")
--- index.py
+++ index.py
@@ -2,3 +2,7 @@ from classes.robot import Robot
 from classes.human import Human
 
 print('Ready to make some robots!')
+
+r2d2 = Robot(name="r2d2", operating_system="rebel_os", color="blue and silver")
+
+# we stopped here with the -i
