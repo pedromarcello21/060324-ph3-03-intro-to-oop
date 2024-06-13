0f90def4fd5ad498a0bcd512e68637317647fca8
brewchetta <chett.tiller@gmail.com>
2024-06-13 11:42:21 -0400

    reset for lecture, add license

--- classes/human.py
+++ classes/human.py
@@ -1,23 +1,2 @@
 class Human:
-
-    def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
-        self._age = 0
-
-    def __repr__(self):
-        return f"Human(first_name={self.first_name}, last_name={self.last_name})"
-    
-    def say_full_name(self):
-        return self.first_name + " " + self.last_name
-    
-    @property
-    def age(self):
-        return self._age
-    
-    @age.setter
-    def age(self, value):
-        raise ValueError('Quit lying about your age!')
-
-    def happy_birthday(self):
-        self._age = self.age + 1
+    pass
--- index.py
+++ index.py
@@ -3,6 +3,4 @@ from classes.human import Human
 
 print('Ready to make some robots!')
 
-r2d2 = Robot(name="r2d2", operating_system="rebel_os", color="blue and silver")
-
 # we stopped here with the -i
